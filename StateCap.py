ac338aeb1ae38167bd0b4b8a77fb3dd5f2fec038
shiri naftali <shirinaftali@gmail.com>
2021-08-15 17:33:43 +0300

    code fixing1

--- StateCap.py
+++ StateCap.py
@@ -1,19 +1,74 @@
-"""We have an existing dictionary that maps US states to their capitals.
-1. Print the state capital of Idaho
-2. Print all states.
-3. Print all capitals.
-4. Create a single string 'Alabama -> Montgomery, Alaska -> Juneau, ...'
+""""
 5. Ensure the string you created in 4. is alphabetically sorted by state
 7. Now we want to add the reverse look up, given the name of a capital what state
 is it in?
 Implement the function def get_state(capital): below so it returns the state.
 GOTCHAS: What happens if two states have the same capital name, how do you
-handle that?
+handle that?"""
 
-"""
-import sys
+def capital_of_Idaho():
+    cap_idaho = STATES_CAPITALS["Idaho"]
+    print(f"idaho capital is {cap_idaho}")
+    return cap_idaho
 
-import pytest
+
+def all_states():
+    print(STATES_CAPITALS.keys())
+    pass
+2
+def all_capitals():
+    print(STATES_CAPITALS.values())
+    pass
+
+def states_capitals_string():
+
+    NEW_STATES_CAPITALS = str(STATES_CAPITALS)
+    print(NEW_STATES_CAPITALS.replace(":", "->"))
+    pass
+
+def get_state(capital):
+
+    for key, value in STATES_CAPITALS.items():
+        if capital == value:
+            return key
+
+    return "key doesn't exist"
+
+
+def test_state_to_capital():
+    assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
+
+
+def test_state_to_capital_unknown():
+    with pytest.raises(KeyError):
+        STATES_CAPITALS['']
+
+
+def test_capital_to_state():
+    assert 'Wyoming' == get_state('Cheyenne')
+
+
+def test_capital_to_state_unknown():
+    with pytest.raises(KeyError):
+        get_state('')
+
+
+def main():
+    return pytest.main(__file__)
+
+
+
+User_choose = str("")
+User_choose = input("""please chose from below and press the number that
+        1. Print the state capital of Idaho
+        2. Print all states
+        3. Print all capitals.
+        4. Create a single string 
+        5. Alphabetically sorted by state
+        7. Enter a capital and find the state
+        0. Exit  
+        """)
+
 
 STATES_CAPITALS = {
     'Alabama' : 'Montgomery',
@@ -69,50 +124,20 @@ STATES_CAPITALS = {
 }
 
 
-def capital_of_Idaho():
-    # Your code here
-    pass
-
-def all_states():
-    # Your code here
-    pass
-
-def all_capitals():
-    # Your code here
-    pass
-
-def states_capitals_string():
-    # Your code here
-    pass
+if User_choose == "1":
+    capital_of_Idaho()
+if User_choose == "2":
+    all_states()
+if User_choose == "3":
+    all_capitals()
+if User_choose == "4":
+    states_capitals_string()
+if User_choose == "5":
+    print("!")
+if User_choose == "7":
+    capital = input("for which capital you asking about the state name: ")
+    print(get_state(capital))
 
 
 
-def get_state(capital):
-    pass
 
-
-
-def test_state_to_capital():
-    assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
-
-
-def test_state_to_capital_unknown():
-    with pytest.raises(KeyError):
-        STATES_CAPITALS['']
-
-
-def test_capital_to_state():
-    assert 'Wyoming' == get_state('Cheyenne')
-
-
-def test_capital_to_state_unknown():
-    with pytest.raises(KeyError):
-        get_state('')
-
-
-def main():
-    return pytest.main(__file__)
-
-
-if __name__ == '__main__':
-    sys.exit(main())
--- strings.py
+++ strings.py
@@ -9,19 +9,51 @@ Create a function that returns a list of 4 character strings:
 Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 ### git comment
 """
-import pytest
+
+#import pytest
 
 def no_duplicates(a_string):
-    pass
+    new_a_string1 = a_string.replace(" ","")
+    new_a_string = sorted(new_a_string1)
+    ni = len(new_a_string)
+    i = 0
+    for i in range(0, ni-8):
+        if new_a_string[i] == new_a_string[i+1]:
+            new_a_string.pop(i)
+            ##print(new_a_string)
+        i+=1
+
+    print(''.join(new_a_string))
 
 
+#####################################################
 def reversed_words(a_string):
-    pass
+
+    new1= a_string.split()
+    j = len(new1)
+    new_a_string = []
+    i = 1
+    for i in range(0,j):
+        x=j-i-1
+        new_a_string.append(new1[x])
+        i+=1
+    j+=1
+    print(new_a_string)
 
 
+##################################################################
 def four_char_strings(a_string):
-    pass
+    new_a_string = []
+    i=0
+    for i in range(i, len(a_string)-4, 4):
+        x=i+4
+        z = a_string[i:x] + " "
+        new_a_string.append(z)
+    print(new_a_string)
 
+
+
+#################################
 
 def test_no_duplicates():
     s = 'monty pythons flying circus'
@@ -37,11 +69,19 @@ def test_four_char_strings():
     s = 'monty pythons flying circus'
     assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 
-
+""""
 def main():
     return pytest.main(__file__)
 
 
 if __name__ == '__main__':
     main()
-    
+"""
+
+
+s_word = ("monty pythons flying circus")
+no_duplicates(s_word)
+reversed_words(s_word)
+four_char_strings(s_word)
+
+
